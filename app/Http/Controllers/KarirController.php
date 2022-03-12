c8fd5ad6733c765bf217f2add3f48920d410d6b3
Jaenal Mustakim <jaenalbandwidht5@gmail.com>
2022-03-12 17:40:20 +0700

    Halaman Karir udah bisa tambah dan hapus data, tinggal edit

--- app/Http/Controllers/KarirController.php
+++ app/Http/Controllers/KarirController.php
@@ -8,11 +8,11 @@ use Illuminate\Http\Request;
 
 class KarirController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $karirs = Karir::count();
